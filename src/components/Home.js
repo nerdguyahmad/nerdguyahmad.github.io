2be0de919064557a5c1ac7441a77cb22ae3bffd6
Izhar Ahmad <thisisizharahmad@gmail.com>
2022-11-28 22:07:09 +0500

    Use text file for storing about me content

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,10 +1,22 @@
+import { useState, useEffect } from 'react';
 import ReactTypingEffect from 'react-typing-effect';
 import PageIntro from "./PageIntro"
 import Section from './Section';
 import backgroundImage from "../assets/home_pageintro_background.jpg";
 import logo from '../assets/logo.png';
+import rawAbout from '../consts/about.txt';
 
 export default function Home () {
+    let [about, setAbout] = useState(null);
+
+    useEffect(() => {
+        async function getAbout() {
+            const aboutContent = await (await fetch(rawAbout)).text();
+            setAbout(aboutContent);
+        }
+        getAbout();
+    }, [])
+
     return (
         <>
             <PageIntro
@@ -26,18 +38,7 @@ export default function Home () {
             />
             <Section
                 title="About Me"
-                body={
-                    `Hi! I'm Izhar Ahmad often known as "Izxxr" on the internet. I'm a student who `+
-                    `is also a passionate developer. My interests are networking, backend web development `+
-                    `and automation. I'm a proficient in Python and JavaScript with some experience `+
-                    `in various languages such as Rust and Go. I like spending hours automating tasks `+
-                    `that could've been achieved in matter of minutes.\n\n`+
-                    `Due to my interest in automation, I also create bots on Discord and various other `+
-                    `platforms to automate basic tasks. I have created many bots on Discord for community and personal `+
-                    `servers that are serving thousands of users.\n\n`+
-                    `I'm also a freelancer who is open for commissions and will be glad to work on any projects `+
-                    `related to backend web development, automation or bot development. Feel free to contact me to discuss regarding this.`
-                }
+                body={about}
                 image={logo}
             />
         </>
